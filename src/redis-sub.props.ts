4a450142c66e8da2e2db222b7c1001c665f557a5
circle 2π <circle2jt@gmail.com>
2023-10-30 09:42:27 +0000

    refactor: Integrate with latest ymlr version

--- src/redis-sub.props.ts
+++ src/redis-sub.props.ts
@@ -1,13 +1,12 @@
 import { RedisOptions } from 'ioredis'
-import { JobProps } from 'ymlr/src/components/.job/job.props'
 import { ElementProxy } from 'ymlr/src/components/element-proxy'
 import { Redis } from './redis'
 
-export type RedisSubProps = {
+export interface RedisSubProps {
   redis?: ElementProxy<Redis>
   uri?: string
   type?: 'text' | 'buffer'
   opts?: RedisOptions
   channels?: string[]
   channel?: string
-} & JobProps
+}
